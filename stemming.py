022ad2a98b46f28e070e2e1fe0883d58f51c27a3
OnurSefa <onursefa_ozcibik@hotmail.com>
2021-12-08 17:09:02 +0300

    Stemming strategy 4 (final)

--- stemming.py
+++ stemming.py
@@ -111,6 +111,29 @@ def train_strategy3(file_path='tr_boun-ud-train.conllu'):
     return result
 
 
+def train_strategy4(file_path='tr_boun-ud-train.conllu'):
+    x_values, y_values = prepare_data(file_path)
+
+    suffixes = []
+    dictionary = []
+    for x, y in zip(x_values, y_values):
+        try:
+            y_length = len(y)
+            x_length = len(x)
+        except TypeError:
+            continue
+        if x_length <= y_length:
+            dictionary.append(x)
+            continue
+        suffix = x[y_length:x_length]
+        word = x[:y_length]
+        dictionary.append(word)
+        suffixes.append(suffix)
+
+    suffixes.sort(key=lambda a:len(a), reverse=True)
+    return suffixes, dictionary
+
+
 def test(test_file_path='tr_boun-ud-test.conllu', train_file_path='tr_boun-ud-train.conllu'):
     suffixes = train(train_file_path)
     # suffixes = train_strategy2(train_file_path)
@@ -143,6 +166,49 @@ def test(test_file_path='tr_boun-ud-test.conllu', train_file_path='tr_boun-ud-tr
         i += 1
         found_x.append(x)
         if i % 1000 == 0:
+            print(i)
+
+    return found_x, y_values, compare
+
+
+def test_strategy4(test_file_path='tr_boun-ud-test.conllu', train_file_path='tr_boun-ud-train.conllu'):
+    suffixes, dictionary = train_strategy4(train_file_path)
+    x_values, y_values = prepare_data(test_file_path)
+    x_values = x_values[:200]
+    y_values = y_values[:200]
+
+    x_count = len(x_values)
+    i = 0
+    found_x = []
+    compare = []
+    while i < x_count:
+        x = x_values[i]
+        old_x = x
+        neu_x = x
+
+        if old_x in dictionary:
+            found_x.append(old_x)
+            compare.append((old_x, old_x))
+            i += 1
+            continue
+
+        for suffix in suffixes:
+            s_length = len(suffix)
+            x_length = len(x)
+
+            if s_length >= x_length - 1:
+                continue
+            last_part = x[x_length-s_length:]
+
+            if last_part == suffix:
+                neu_x = x[:x_length-s_length]
+                if neu_x in dictionary:
+                    x = x[:x_length-s_length]
+                    break
+        compare.append((old_x, neu_x))
+        i += 1
+        found_x.append(x)
+        if i % 100 == 0:
             print(i)
 
     return found_x, y_values, compare
@@ -242,7 +308,7 @@ def evaluate(x_values, y_values):
 
 
 if __name__ == '__main__':
-    x_result, y_result, compare = test_strategy3()
+    x_result, y_result, compare = test_strategy4()
     a = evaluate(x_result, y_result)
     print('accuracy:', a)
     print('a')
